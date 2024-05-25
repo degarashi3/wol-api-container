e339db646871bb6f7884806e6006cad438f3a59b
degarashi3 <debug.log@gmail.com>
2024-05-25 18:19:35 +0900

    first commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from wakeonlan import send_magic_packet
+
+app = FastAPI()
+
+@app.get("/")
+def read_items(macaddr: str):
+    print(macaddr)
+    send_magic_packet(macaddr)
+    return {'send magic packet to ': macaddr}
