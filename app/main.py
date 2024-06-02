95d0f3b9604b6aeff9ed06e53d7169c912a5d9ce
degarashi3 <debug.log@gmail.com>
2024-06-02 18:27:31 +0900

    APIを変更

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from wakeonlan import send_magic_packet
+# from wakeonlan import send_magic_packet
+import subprocess
 
 app = FastAPI()
 
 @app.get("/")
 def read_items(macaddr: str):
-    print(macaddr)
-    send_magic_packet(macaddr)
+    command = 'sudo etherwake -i wlan0 ' + macaddr
+    ret = subprocess.run(command, shell=True)
+    # send_magic_packet(macaddr)
     return {'send magic packet to ': macaddr}
