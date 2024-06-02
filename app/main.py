29bac2add57fee3c817816e470b8ab3325c11de5
degarashi3 <debug.log@gmail.com>
2024-06-02 18:43:30 +0900

    debug

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-# from wakeonlan import send_magic_packet
+from wakeonlan import send_magic_packet
 import subprocess
 
 app = FastAPI()
@@ -9,5 +9,5 @@ app = FastAPI()
 def read_items(macaddr: str):
     command = 'sudo etherwake -i wlan0 ' + macaddr
     ret = subprocess.run(command, shell=True)
-    # send_magic_packet(macaddr)
+    send_magic_packet(macaddr,interface='wlan0')
     return {'send magic packet to ': macaddr}
